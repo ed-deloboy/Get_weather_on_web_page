8fa0366d5c323725eac840040789829ddbc6329d
Eduard <edone.olimp@gmail.com>
2021-11-08 00:50:58 +0300

    Added on page: wind and date

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const param = {
     "appid": "450cfc6fe3c5e81e1717f2d1bab2ec0d",
     "lang": "ru"
 }
-
+let dateNow = new Date().toLocaleDateString();
+let newSelectContainer = document.querySelector('.newselect');
 
 function getWeather() {
     const cityId = document.querySelector('#city').value;
@@ -12,15 +13,20 @@ function getWeather() {
             return weather.json();
         }).then(showWeather);
 }
-
-
 // вывод данных
 function showWeather(data) {
     console.log(data);
 
     document.querySelector('.cityname').textContent = data.name;
     document.querySelector('.windweather').textContent = data.weather[0].description;
-    document.querySelector('.temperaturecity').textContent = Math.round(data.main.temp);
+    document.querySelector('#temperaturecity').textContent = Math.round(data.main.temp);
+    document.querySelector('#weatheimage').innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+    document.querySelector('#windwspeed').textContent = Math.round(data.wind.speed);
+    // date
+    document.querySelector('#weather-hero__time').textContent = dateNow;
+    var newSelect = document.createElement("select");
+    newSelect.innerHTML = "<option>Привет!</option>";
+    newSelectContainer.appendChild(newSelect);
 
 
 }
